3eee5e6d957131b3dfc1d487a64e6e7c31fae853
Chance Penner <chance.penner2016@gmail.com>
2019-12-09 21:57:13 -0600

    add print weekdays functionality

--- calendar.hs
+++ calendar.hs
@@ -16,7 +16,16 @@ data ListOfMonths = January | February | March
           | October | November | December
             deriving (Show, Eq, Ord, Enum)
 
+
+weekDays = ["Su","Mo","Tu","We","Th","Fr","Sa"]
 allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+printDays :: Int -> IO ()
+printDays 7 = return ()
+printDays n =
+  do
+    putStr $  (weekDays!!(n) ++ " ")
+    printDays(n+1)
 
 --returns integer of first day of the month of specified year. Monday = 1 .. Sunday = 7
 firstDayOfMonth :: Integer -> Int -> Int
@@ -29,4 +38,6 @@ printCalendar year chosenMonth =
     else do
       putStrLn $ show (firstDayOfMonth year chosenMonth)
 
-      putStrLn $ show (allMonths!!(chosenMonth-1))
+      putStrLn $ "\t" ++ allMonths!!(chosenMonth-1)
+      printDays 0
+      putStrLn "\n"
